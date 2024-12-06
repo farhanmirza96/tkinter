fd9b94446be46395c7953804de64866f109b2147
farhanmirza96 <farhanmirza96@gmail.com>
2024-12-06 16:40:47 +0500

    TKINTER

---
+++ database.py
@@ -0,0 +1,2 @@
+def insert():
+    print('Insert')
--- ems.py
+++ ems.py
@@ -1,16 +1,23 @@
 from customtkinter import *
 from PIL import Image
-from tkinter import ttk
+from tkinter import ttk, messagebox
+import database as db
+
+def add_employee():
+    if idEntry.get()=='' or nameEntry.get()=='' or phoneEntry.get()=='' or salaryEntry.get()=='':
+        messagebox.showerror('Alert', 'All feild are required')
+    else:
+        db.insert(idEntry.get(), nameEntry.get(), phoneEntry.get(), salaryEntry.get(), role_options.get(), gender_options.get())
 
 window = CTk()
-window.geometry('930x580')
+window.geometry('950x580+100+100')   # Here 100 is the x position and 100 is the y position of the window to apear on the screen
 window.resizable(0,0)
 
 window.title('Employee Management System')
 
 window.configure(fg_color = '#161c30')
 
-image = CTkImage(Image.open('employee.jpg'), size=(930,158))
+image = CTkImage(Image.open('employee.jpg'), size=(950,158))
 imageLabel = CTkLabel(window, image=image, text='')
 imageLabel.grid(row=0, column=0, columnspan=2)
 
@@ -59,14 +66,64 @@ salaryEntry = CTkEntry(leftFrame,width=140,height=28, corner_radius=0,border_wid
 salaryEntry.grid(row=5, column=1, sticky='W')
 
 rightFrame = CTkFrame(window)
-rightFrame.grid(row=1, column=1)
+rightFrame.grid(row=1, column=1, pady= 5)
 
 search_options = ['ID', 'Employee Name', 'Phone', 'Role', 'Gender', 'Salary']
 searchCombo = CTkComboBox(rightFrame, width=140, font=('garamond',15), values=search_options, corner_radius=0, state='readonly')
 searchCombo.set('Search By')
-searchCombo.grid(row=0, column=0, padx=(0,20), pady=(5,5), sticky='W')
+searchCombo.grid(row=0, column=0, sticky='W')
 
 searchEntry = CTkEntry(rightFrame,width=140,height=28, corner_radius=0,border_width=2,bg_color='white',placeholder_text='Enter your Salary.',font=('garamond',15,))
-searchEntry.grid(row=0, column=1, sticky='W', padx=(0,20))
+searchEntry.grid(row=0, column=1, sticky='W')
+
+searchButton = CTkButton(rightFrame, text='Search', width=100)
+searchButton.grid(row=0, column=2)
+
+showAllBtn = CTkButton(rightFrame, text='Show All', width=100)
+showAllBtn.grid(row=0, column=3, pady=5)
+
+tree = ttk.Treeview(rightFrame, height=13)
+tree.grid(row=1, column=0, columnspan=4)
+
+tree['columns'] = ('Id', 'Name', 'Phone', 'Role', 'Gender', 'Salary')
+tree.heading('Id', text='Id')
+tree.heading('Name', text='Name')
+tree.heading('Phone', text='Phone')
+tree.heading('Role', text='Role')
+tree.heading('Gender', text='Gender')
+tree.heading('Salary', text='Salary')
+
+tree.config(show='headings')   # it will show only headings not the first column by default
+
+tree.column('Id', anchor='center', width=50)   # dy default anchor is in center
+tree.column('Name', width=150)
+tree.column('Phone', width=100)
+tree.column('Role', width=110)
+tree.column('Gender', width=100)
+tree.column('Salary', width=100)
+
+style = ttk.Style()
+style.configure("Treeview", font=('arial',18,'bold'))
+
+scrollbar = ttk.Scrollbar(rightFrame, orient=VERTICAL)
+scrollbar.grid(row=1, column=4, sticky='ns')
+
+btnFrame = CTkFrame(window, fg_color = '#161c30')
+btnFrame.grid(row=2, column=0, columnspan=2, pady=5)
+
+newbtn = CTkButton(btnFrame, text='New Employee', width=160, font=('arial',18,'bold'), corner_radius=15)
+newbtn.grid(row=0, column=0, pady=5)
+
+addbtn = CTkButton(btnFrame, text='Add Employee', width=160, font=('arial',18,'bold'), corner_radius=15, command=add_employee)
+addbtn.grid(row=0, column=1, padx=5, pady=5)
+
+updatebtn = CTkButton(btnFrame, text='Update', width=160, font=('arial',18,'bold'), corner_radius=15)
+updatebtn.grid(row=0, column=2, padx=5, pady=5)
+
+deletebtn = CTkButton(btnFrame, text='Delete', width=160, font=('arial',18,'bold'), corner_radius=15)
+deletebtn.grid(row=0, column=3, padx=5, pady=5)
+
+deleteAllbtn = CTkButton(btnFrame, text='Delete', width=160, font=('arial',18,'bold'), corner_radius=15)
+deleteAllbtn.grid(row=0, column=4, padx=5, pady=5)
 
 window.mainloop()
